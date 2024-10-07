0a6e53037bf43e610f3e31010a85b7835d1c04b9
ssmunir <munirsalihu@yahoo.com>
2024-10-07 14:13:26 -0400

    Merged and summed rows with same country name and added continent column

--- merger.py
+++ merger.py
@@ -2,6 +2,8 @@
 
 import os
 import dask.dataframe as dd
+import pandas as pd
+import pycountry_convert as pc 
 
 # Define the folder containing the CSV files
 folder_path = r"C:\Users\auuser\Documents\Munir\Urbanization Analysis\GEE\Population data"
@@ -29,8 +31,30 @@ for i, filename in enumerate(os.listdir(folder_path)):
 # Concatenate all DataFrames
 merged_df = dd.concat(dataframes, axis=1)
 
-# Compute the final merged result and save it to a new CSV file
-output_path = os.path.join(folder_path, 'merged_population_data_rounded.csv')
-merged_df.compute().to_csv(output_path, index=False)
+# Merge rows with the same country name and sum their values.
+merged_df = merged_df.groupby('country').sum().reset_index()
 
-print(f'Merged and rounded CSV saved at: {output_path}')
+# Compute the final merged result and save it to a new CSV file
+output_path = os.path.join(folder_path, 'full_population_data.csv')
+#merged_df.compute().to_csv(output_path, index=False)
+
+#print(f'Merged and rounded CSV saved at: {output_path}')
+
+data = merged_df.compute()
+
+
+def country_to_continent(country_name):
+    try:
+        country_alpha2 = pc.country_name_to_country_alpha2(country_name)
+        country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+        country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
+        return country_continent_name
+    except KeyError as e:
+        return "None"
+ 
+
+# Example
+country_name = data['country']
+continent_name = [country_to_continent(i) for i in country_name]
+
+data.insert(1, "continent", continent_name)
