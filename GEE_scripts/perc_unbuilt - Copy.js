29200c4e1097e5be006b6d47504b2281a450cd2d
tannerregan <54477992+tannerregan@users.noreply.github.com>
2025-03-26 14:09:42 -0400

    try to run built with USA

--- GEE_scripts/perc_unbuilt - Copy.js
+++ GEE_scripts/perc_unbuilt - Copy.js
@@ -1,9 +1,18 @@
 /**** REVISED SCRIPT FOR REALISTIC UNBUILT PERCENTAGE DISTRIBUTION ****/
 // Load datasets
 var ghs = ee.Image('JRC/GHSL/P2023A/GHS_BUILT_S/2025').select('built_surface');
-var countriesFC = ee.FeatureCollection('FAO/GAUL/2015/level0');
-//var countriesFC = ee.FeatureCollection('FAO/GAUL_SIMPLIFIED_500m/2015/level0');
-var nigeria = countriesFC.filter(ee.Filter.eq('ADM0_NAME', 'Nigeria')).geometry();
+//var countriesFC = ee.FeatureCollection('FAO/GAUL/2015/level0');
+//var countriesFC = ee.FeatureCollection('UN/Geodata/BNDA_simplified/current');
+var countriesFC = ee.FeatureCollection('FAO/GAUL_SIMPLIFIED_500m/2015/level0');
+var country = countriesFC.filter(ee.Filter.eq('ADM0_NAME', 'United States of America')).geometry();
+
+//Print out projections
+print(ghs.projection())
+print(countriesFC.first().geometry().projection())
+
+//vizualize
+var viz = {min:0, max:100, palette:['ffffff','0000FF']};
+Map.addLayer(ghs.updateMask(ghs.neq(0)),viz, 'raw GHS Built');
 
 // Keep only pixels 0..10000, mask out anything else (like 65535)
 var validMask = ghs.gte(0).and(ghs.lte(10000));
@@ -13,6 +22,7 @@ var ghsMasked = ghs.updateMask(validMask);
 var percentBuilt100m = ghsMasked.divide(100);
 
 // Aggregate to 1km using average percentage
+print(percentBuilt100m.projection())
 var avgPercentBuilt1km = percentBuilt100m
   .reduceResolution({
     reducer: ee.Reducer.mean(),
@@ -21,25 +31,24 @@ var avgPercentBuilt1km = percentBuilt100m
   .reproject({
     crs: ghsMasked.projection(),
     scale: 1000
-  })
-  .clip(nigeria);
+  });
+  //  .clip(country)
   
 // Calculate percentage unbuilt (0-100 scale)
 var percentUnbuilt1km = ee.Image.constant(100).subtract(avgPercentBuilt1km);
 // Round to nearest whole percentage for more intuitive grouping
 var roundedPercentUnbuilt = percentUnbuilt1km.round();
 
-
-// create vizualization parameters
-var viz = {min:0, max:100, palette:['d600ff','ffffff']};
-Map.addLayer(roundedPercentUnbuilt.updateMask(roundedPercentUnbuilt.neq(100)),viz, 'GHS_UNBUILT_S1km');
-
+//vizualize
+var viz = {min:0, max:100, palette:['0000FF','ffffff']};
+Map.addLayer(roundedPercentUnbuilt.updateMask(roundedPercentUnbuilt.neq(100)),viz, 'GHS_UNBUILT');
 
 
+/*
 // Get frequency histogram
 var histDict = roundedPercentUnbuilt.reduceRegion({
   reducer: ee.Reducer.frequencyHistogram(),
-  geometry: nigeria,
+  geometry: country,
   scale: 1000,
   bestEffort: true,
   maxPixels: 1e9,
@@ -71,9 +80,9 @@ var freqFC = ee.FeatureCollection(feats);
 // Export the table
 Export.table.toDrive({
   collection: freqFC,
-  description: 'Nigeria_1km_Percent_Unbuilt_Revised',
+  description: 'country_1km_Percent_Unbuilt_Revised',
   fileFormat: 'CSV',
   selectors: ['percent_unbuilt', 'frequency']
 });
 
-
+*/
